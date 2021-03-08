0a45e812062d47f1abccca24845c54d32fc869d2
Brian Thomas <bthomas@imaginuity.com>
2021-03-08 16:11:15 -0600

    added content feature image redo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import ContentWithFeaturedImage from "./components/ContentWithFeaturedImage/ContentWithFeaturedImage";
+import ContentWithFeaturedPhone from "./components/ContentWithFeaturedPhone/ContentWithFeaturedPhone";
 import ThreeColCta from "./components/ThreeColCta/ThreeColCta";
 import ImageSlider from "./components/ImageSlider/ImageSlider";
 import CtaSlider from "./components/CtaSlider/CtaSlider";
@@ -15,13 +16,13 @@ function App() {
     <>
       <Header />
       <Hero />
-      <ContentWithFeaturedImage />
+      <ContentWithFeaturedPhone />
       <ThreeColCta />
       <ImageSlider />
       <CtaSlider />
       {/* testimonals w/slick slider on mobile */}
       <CtaForm />
-      {/* Content with featured image */}
+      <ContentWithFeaturedImage />
       <Footer />
     </>
   );
--- src/components/ContentWithFeaturedImage/ContentWithFeaturedImage.jsx
+++ src/components/ContentWithFeaturedImage/ContentWithFeaturedImage.jsx
@@ -6,7 +6,7 @@ import "./contentWithFeaturedImage.scss";
 const ContentWithFeaturedImage = () => {
   return (
     <section className="featured-image-container">
-      <Container>
+      <Container className="desktop d-none d-md-block">
         <Row>
           <Col xs={12} md={6} className="img-container">
             <img
@@ -20,31 +20,41 @@ const ContentWithFeaturedImage = () => {
           </Col>
           <Col xs={12} md={6} className="content-container">
             <div className="content-wrapper">
-              <h3 className="title">Control your home from your phone</h3>
-              <p>Interact with your home wherever you are: </p>
-              <ul>
-                <li>
-                  <AiOutlineCheck size={25} />{" "}
-                  <span>
-                    Manage your security system, locks, cameras, lights and more
-                  </span>
-                </li>
-                <li>
-                  <AiOutlineCheck size={25} />{" "}
-                  <span>
-                    Receive motion and security alerts directly to your phone
-                  </span>
-                </li>
-                <li>
-                  <AiOutlineCheck size={25} />{" "}
-                  <span>
-                    See and speak to visitors through your camera via the app
-                  </span>
-                </li>
-              </ul>
+              <h3 className="title">
+                <span className="check">
+                  <AiOutlineCheck size={50} />
+                </span>
+                Protecting over 1 million people
+              </h3>
             </div>
           </Col>
         </Row>
+      </Container>
+      <Container className="d-xs-block d-md-none">
+        <div className="mobile">
+          <Row>
+            <Col xs={12} className="content-container">
+              <div className="content-wrapper">
+                <h3 className="title">
+                  <span className="check">
+                    <AiOutlineCheck size={50} />
+                  </span>
+                  Protecting over 1 million people
+                </h3>
+              </div>
+            </Col>
+            <Col xs={12} className="img-container">
+              <img
+                src="https://images.ctfassets.net/n58cc9djl3c5/1Yxwjz4jc741GIxpsSZCFr/337fe1add7177431795500ef54ae09e2/Newsweek_BISCS2020_Siegel_Standard.png?fm=png&fl=png8"
+                alt="best_in_state pic"
+              />
+              <p>
+                Brinks Home <br /> best-in-state <br /> customer service
+                <small>Newsweek, 2020</small>
+              </p>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </section>
   );
